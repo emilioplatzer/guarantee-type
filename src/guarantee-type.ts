d1f24d663e9350c17abb70d3391a349e04daebe4
Emilio <emilioplatzer@gmail.com>
2022-02-10 16:38:59 -0300

    Agrego objetos planos tipo mapa de valores.

--- src/guarantee-type.ts
+++ src/guarantee-type.ts
@@ -1,10 +1,31 @@
-export type Description = 'string'|'number'
 
-export type GuaranteedType<CurrentD extends Description> =
-    CurrentD extends 'string' ? string :
-    never;
+export type Values = 'string'|'number'|'boolean'|'bigint'|'symbol'
 
-export function guarantee<CurrentD extends Description>(description:CurrentD, value:any):GuaranteedType<CurrentD>{
-    if ( description == 'string' && typeof value != 'string') throw new Error(`guarantee excpetion. Value is not proper type`);
+export type Description = {type:Values} | {[k in keyof {}] : Values}
+
+export type GuaranteedType1<CurrentD> = {[K in keyof CurrentD] : GuaranteedType<CurrentD[K]>}
+
+export type Opts = any;
+
+export type GuaranteedType<CurrentD> = 
+    CurrentD extends { string : Opts } ? string  :
+    CurrentD extends { number : Opts } ? number  :
+    CurrentD extends { boolean: Opts } ? boolean :
+    // CurrentD extends infer T[keyof T] ?  {[k in keyof T] : GuaranteedType<K[T]>} :
+    // CurrentD extends { [K in keyof CurrentD]: infer S} ?  {[K in keyof CurrentD] : GuaranteedType<CurrentD[K]>} :
+    CurrentD extends { object: infer T} ? {[K in keyof T] : GuaranteedType<T[K]>} :
+    unknown
+// */    
+
+
+var s: GuaranteedType<{value:'string'}>;
+var n: GuaranteedType<{value:'number'}>;
+var u: GuaranteedType<{value:string}>;
+
+export function guarantee<CurrentD>(description:CurrentD, value:any):GuaranteedType<CurrentD>{
+    // @ts-expect-error We know that description can't be == 'object'
+    if ( description == 'object' ) throw new Error(`guarantee excpetion. Invalide description "object"`);
+    // @ts-ignore
+    if ( value && description != typeof value ) throw new Error(`guarantee excpetion. Value is not proper type`);
     return value;
 }
--- test/test.ts
+++ test/test.ts
@@ -1,22 +1,59 @@
-import { Description, guarantee } from "../src/guarantee-type";
+import { Description, Opts, guarantee } from "../src/guarantee-type";
 
-import * as assert from "assert"
+import * as assert from "assert";
+
+var opts:Opts;
 
 describe("guarantee",function(){
-    it("string", function(){
-        var result:string; 
-        var value:any = "any string";
-        result = guarantee("string", value);
-        assert.equal(result, value);
-    })
-    it("string returns string", function(){
-        var result:number; 
-        var value:any = "any string";
-        // @ts-expect-error
-        result = guarantee("string", value);
-    })
-    it("number cannot be assigned to string", function(){
-        var value:any = 43;
-        assert.throws(()=>guarantee("string", value), /guarantee excpetion. Value is not proper type/);
+    describe("values",function(){
+        it("string", function(){
+            var result:string; 
+            var value:any = "any string";
+            result = guarantee({string:opts}, value);
+            assert.equal(result, value);
+        })
+        it("detects TypeError string cannot be asigned to number", function(){
+            var result:number; 
+            var value:any = "any string";
+            // @ts-expect-error
+            result = guarantee({string:opts}, value);
+            // @ts-expect-error
+            var results:string = guarantee({number:opts}, value);
+        })
+        it("number cannot be assigned to string", function(){
+            var value:any = 43;
+            assert.throws(()=>guarantee({string:opts}, value), /guarantee excpetion. Value is not proper type/);
+        })
+    });
+    describe("objects like Record<string, value>", function(){
+        var description1 = {object: {
+            name: { string : opts },
+            age:  { number : opts },
+            ready:{ boolean: opts },
+        }}
+        type Type1 = {
+            name: string
+            age: number
+            ready: boolean
+        }
+        var value1 = {
+            name: 'Abigail',
+            age: 72,
+            ready: true,
+        }
+        type Type2 = {
+            name: string
+            born: Date
+        }
+        it("detects TypeError", function(){
+            var result: Type2
+            // @ts-expect-error
+            result = guarantee(description1, value1);
+        })
+        it("receive a Record<string, value>", function(){
+            var result: Type1
+            result = guarantee(description1, value1 );
+            assert.equal(result, value1);
+        })
     })
 })
